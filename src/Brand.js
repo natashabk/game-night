c3b1486ca4b2c9687ad7bf7576e0bb1df41870cb
natashabk <natasha.k@hey.com>
2021-09-29 11:19:34 -0700

    updated state check again

--- src/Brand.js
+++ src/Brand.js
@@ -17,7 +17,7 @@ const subtitleStyle = { color: 'white', padding: 5, borderRadius: 3 }
 
 const Brand = ( { gameMode } ) => {
   const err = useSelector( state => state.error );
-  const room = useSelector(state => state.room);
+  const pastLogin = useSelector(state => state.username);
 
   const subtitle = () => {
     if ( err ) {
@@ -25,8 +25,9 @@ const Brand = ( { gameMode } ) => {
     } else return null
   }
 
-  if ( gameMode || room) return <Logo style={{ height: 40, width: 40, margin: '-10px 0px 10px' }} />
-    
+  if ( gameMode || pastLogin ) {
+    return <Logo style={{ height: 40, width: 40, margin: '-10px 0px 10px' }} />
+  }
 
   return (
     <Header style={{ ...headerStyle, zIndex: 1 }}>
